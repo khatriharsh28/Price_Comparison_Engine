a06b42707a4f3fad516289af2e407c3bd888c5f2
khatriharsh28 <khatriharsh281@gmail.com>
2022-07-21 10:44:51 +0530

    Buttons added

--- SPASH.py
+++ SPASH.py
@@ -13,17 +13,7 @@ root = Tk()
 root.geometry("654x487+450+150")
 root.config(bg='grey')
 
-# canvas = Canvas(root, width = 500, height = 500)
-# canvas.pack()
-# #GIF in my_image variable
-# #Give the entire file address along with the file name and gif extension
-# #Use \\ in the address
-# #The image given by me is C:\\UserAdmin\\Device\\Desktop2\\canyon.gif
-# my_image = PhotoImage(file='intro.gif')
-# canvas.create_image(0, 0, anchor = NW, image=my_image)
-
-
-img = PhotoImage(file="1.png")
+img = PhotoImage(file="Loader.png")
 labelimg = Label(root, image=img)
 labelimg.place(x=0, y=0)
 
@@ -54,64 +44,83 @@ class Price_compare:
         height = y2-y1
         canvas['width']=width
         canvas['height']=height
-        fps=40    #Change the fps to make the animation faster/slower
+        fps=150    #Change the fps to make the animation faster/slower
         shift()
 
-        #Image
-        # image = Image.open("LOGO.png")
-        # photo = ImageTk.PhotoImage(image)
+        self.cont = PhotoImage(file="Continue.png")
+        b_continue = Button(master,image = self.cont, borderwidth = 0, command= self.page1)
+        b_continue.place(x = 280, y = 415, width=105,height=29)
 
-        
-        # label1 = Label(root, image = photo)
-        # label1.image = photo
-        # # label.grid(row=1)
+    def vst_grocery(self):   
+        link='https://www.flipkart.com/grocery-supermart-store?marketplace=GROCERY&fm=neo%2Fmerchandising&iid=M_e37b4f0b-2cce-4c19-8394-2e3f61d6dde8_1_372UD5BXDFYS_MC.CBUR1Q46W5F1&otracker=hp_rich_navigation_2_1.navigationCard.RICH_NAVIGATION_Grocery_CBUR1Q46W5F1&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_2_L0_view-all&cid=CBUR1Q46W5F1' 
+        webbrowser.open(link)
 
-        # # img=Image.open(name="IMG", file="LOGO.png")
-        # # photo = ImageTk.PhotoImage(img)
-        # # label1=Label(root, image=img, bd=5, height=450, width=650)
-        # label1.grid(row=0, column=0, padx=20, pady=10)
-        # click_btn= PhotoImage(file='Continue.png')
-        # img_label= Label(image=click_btn)
-        # button= Button(root, image=click_btn,command= self.page1)
-        # button.pack(pady=30)
-        b_continue = Button(master, text = 'Click Me >>',command= self.page1)
-        # b_continue = Button(root, text = 'Click Me !', image = click_btn).pack(side = TOP)
-        b_continue.place(x = 280, y = 415, width=90,height=35)
-        
+    def vst_mobile(self):   
+        link='https://www.flipkart.com/mobile-phones-store?fm=neo%2Fmerchandising&iid=M_cf226ade-2f23-41c4-aade-69644a0b4325_1_372UD5BXDFYS_MC.ZRQ4DKH28K8J&otracker=hp_rich_navigation_3_1.navigationCard.RICH_NAVIGATION_Mobiles_ZRQ4DKH28K8J&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_3_L0_view-all&cid=ZRQ4DKH28K8J' 
+        webbrowser.open(link)
+
+    def vst_fashion(self):   
+        link='https://www.flipkart.com/clothing-and-accessories/topwear/pr?sid=clo,ash&p[]=facets.ideal_for%255B%255D%3DMen&p[]=facets.ideal_for%255B%255D%3Dmen&otracker=categorytree&fm=neo%2Fmerchandising&iid=M_cb605ebf-213e-436c-9731-754da26f74a1_1_372UD5BXDFYS_MC.AHHHWF67UPNB&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Fashion~Men%2527s%2BTop%2BWear~All_AHHHWF67UPNB&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L2_view-all&cid=AHHHWF67UPNB' 
+        webbrowser.open(link)
+
+    def vst_electronis(self):   
+        link='https://www.flipkart.com/audio-video/~cs-53mrbtcuf5/pr?sid=0pm&collection-tab-name=Audio+And+Video&p%5B%5D=facets.fulfilled_by%255B%255D%3DFlipkart%2BAssured&p%5B%5D=facets.availability%255B%255D%3DExclude%2BOut%2Bof%2BStock&p%5B%5D=facets.rating%255B%255D%3D4%25E2%2598%2585%2B%2526%2Babove&fm=neo%2Fmerchandising&iid=M_b1792f37-20d1-4e3d-8f2b-15d3ea7a84e5_1_372UD5BXDFYS_MC.9JGNW7M0TUHD&otracker=hp_rich_navigation_1_1.navigationCard.RICH_NAVIGATION_Electronics~Audio~All_9JGNW7M0TUHD&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_1_L2_view-all&cid=9JGNW7M0TUHD' 
+        webbrowser.open(link)
+
+    def vst_application(self):   
+        link='https://www.flipkart.com/tvs-and-appliances-new-clp-store?fm=neo%2Fmerchandising&iid=M_2a08020e-58fe-4b00-9799-6c44138fa9bf_1_372UD5BXDFYS_MC.LO4IWVHA61BX&otracker=hp_rich_navigation_7_1.navigationCard.RICH_NAVIGATION_Appliances_LO4IWVHA61BX&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_7_L0_view-all&cid=LO4IWVHA61BX' 
+        webbrowser.open(link)
+
     def page1(self):
         self.var = StringVar()
         self.pg1 = Toplevel(root)
         self.pg1.geometry("654x487+450+150")
         self.pg1.title('Price Comparison Engine')
-
-        # frame = Frame(self.pg1, width=654, height=487)
-        # frame.pack() 
-        # frame.place(anchor='center', relx=0.5, rely=0.5)
-        # # Label
-        # # img1 = PhotoImage(file="LOGO1.png")
-        # # labelimg = Label(self.pg1, image=img1)
-        # # labelimg.place(x=0, y=0, width=654,height=487)
-
-        # img = ImageTk.PhotoImage(Image.open("LOGO1.png"))
-
-        # # Create a Label Widget to display the text or Image
-        # label = Label(frame, image = img)
-        # label.pack()
-
+        # self.pg1.config(bg='white')
         label = Label(self.pg1, text='Enter the product :-' , font=("Comic Sans MS", 15))
-        #label.grid(row=3, column=4,padx=(130,10),pady=30)
         label.place(x = 100, y = 80, width=200,height=40)
-        entry = Entry(self.pg1, textvariable=self.var, font=("Comic Sans MS", 15))
-        #entry.grid(row=3, column=5)
+
+        def click(*args):
+            if entry.get()=='Product Name':
+                entry.delete(0, 'end')
+            entry.config(fg='black')
+  
+        entry = Entry(self.pg1, textvariable=self.var, fg='grey', font=("Comic Sans MS", 15))
+        entry.insert(0, 'Product Name')
+        entry.pack(pady=10)
+        entry.bind("<Button-1>", click)
         entry.place(x = 330, y = 80, width=250,height=40)
         
         button_find = Button(self.pg1, text='Find',font=("Comic Sans MS", 15), command=self.find)
-        # button_find.grid(row=4, column=5, sticky=W, pady=8)
         button_find.place(x = 280, y = 150, width=90,height=35)
+        
+        self.g = PhotoImage(file="grocery.png")
+        grocery = Button(self.pg1, text = 'Grocery', image = self.g, compound= TOP, borderwidth = 2, bg='white',command= self.vst_grocery)
+        grocery.pack(side=TOP)
+        grocery.place(x = 60, y = 380, width=90,height=100)
+
+        self.m = PhotoImage(file="mobile.png")
+        mobile = Button(self.pg1, text = 'Mobile', image = self.m, compound= TOP, borderwidth = 2, bg='white',command= self.vst_mobile)
+        mobile.pack(side=TOP)
+        mobile.place(x = 180, y = 380, width=90,height=100)
+
+        self.f = PhotoImage(file="fashion.png")
+        fashion = Button(self.pg1, text = 'fashion', image = self.f, compound= TOP, borderwidth = 2, bg='white',command= self.vst_fashion)
+        fashion.pack(side=TOP)
+        fashion.place(x = 300, y = 380, width=90,height=100)
+
+        self.e = PhotoImage(file="electronics.png")
+        electronics = Button(self.pg1, text = 'Electronics', image = self.e, compound= TOP, borderwidth = 2, bg='white',command= self.vst_electronis)
+        electronics.pack(side=TOP)
+        electronics.place(x = 420, y = 380, width=90,height=100)
+
+        self.a = PhotoImage(file="application.png")
+        application = Button(self.pg1, text = 'Application', image = self.a, compound= TOP, borderwidth = 2, bg='white',command= self.vst_application)
+        application.pack(side=TOP)
+        application.place(x = 540, y = 380, width=90,height=100)
         
 
     def find(self):
-        #page1.destroy()
         self.product = self.var.get()
         self.product_arr = self.product.split()
         self.n = 1
@@ -134,7 +143,6 @@ class Price_compare:
         self.window.title('Price Comparison Engine')
         label_title_flip = Label(self.window, text='Flipkart Title:')
         label_title_flip.grid(row=0, column=0, sticky=W)
-        self.pg1.destroy()
 
         label_flipkart = Label(self.window, text='Flipkart price (Rs):')
         label_flipkart.grid(row=2, column=0, sticky=W)
@@ -180,6 +188,7 @@ class Price_compare:
         except:
             lab_amz = Label(self.window, text='Product not available')
             lab_amz.grid(row=3, column=2, padx=4)
+        self.pg1.destroy()
         
         button_search = Button(self.window, text='Search', command=self.search, bd=4)
         button_search.grid(row=3, column=3, sticky=E, padx=30, pady=40)
@@ -191,7 +200,6 @@ class Price_compare:
         button_flip_visit.grid(row=2, column=2, sticky=W)
 
     def price_flipkart(self, key):
-        # print(str(key))
         url_flip = 'https://www.flipkart.com/search?q=' + str(key) + '&marketplace=FLIPKART&otracker=start&as-show=on&as=off'
         map = defaultdict(list)
 
@@ -199,8 +207,7 @@ class Price_compare:
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         source_code = requests.get(url_flip, headers=self.headers)
         soup = BeautifulSoup(source_code.text, "html.parser")
-        #print(source_code.text)
-        #print(soup)
+        
         self.opt_title_flip = StringVar()
         home = 'https://www.flipkart.com'
         for block in soup.find_all('div', {'class': '_2kHMtA'}):
@@ -211,9 +218,7 @@ class Price_compare:
                 price = p.text[1:]
             for l in block.find_all('a', {'class': '_1fQZEK'}):
                 link = home + l.get('href')
-            # for i in block.find_all('img', {'class': '_396cs4_2amPTt_3qGmMb_3exPp9'}):
-            #     #disp= i.get('src')
-            #     print( i.get('src'))
+                
             map[title] = [price, link]
 
         user_input = self.var.get().title()
@@ -232,42 +237,27 @@ class Price_compare:
     def price_ebay(self, key):
         url_ebay = 'https://www.ebay.com/sch/i.html?_nkw=' + str(key)
                     
-        # Faking the visit from a browser
+                    
         headers = {
             "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
         }
         html = requests.get('https://www.ebay.com/sch/i.html?_nkw='+str(key), headers=headers).text
         soup = BeautifulSoup(html, 'lxml')
-        # print(html)
         
         map = defaultdict(list)
         home = 'https://www.ebay.com'
-        # source_code = requests.get(url_ebay, headers=headers)
-        # plain_text = source_code.text
+        
         self.opt_title = StringVar()
-        # self.soup = BeautifulSoup(plain_text, "html.parser")
-        # print(source_code)
-        #print(self.soup)
+        
         for item in soup.select('.s-item__wrapper.clearfix'):
             title, price, link = None, 'Currently Unavailable', None
             title = item.select_one('.s-item__title').text
             price =  item.select_one('.s-item__price').text
             link = item.select_one('.s-item__link')['href']
-            # print(title,link,price)
+            
             if title!= 'Shop on eBay' and link :
                 map[title] = [price, link]
-        # for html in self.soup.find_all('div', {'class': 's-item__wrapper.clearfix'}):
-        #     title, link = None, None
-        #     for heading in html.find_all('span', {'class': 's-item__title'}):
-        #         title = heading.text
-        #     for p in html.find_all('span', {'class': 's-item__price'}):
-        #         price = p.text[1:]
-        #     for l in html.find_all('a', {'class': 's-item__link'}):
-        #         link = home + l.get('href')
-        #     print(title,link,price)
-        #     if title and link:
-        #         map[title] = [price, link]
         
         user_input = self.var.get().title()
         self.matches_ebay = get_close_matches(user_input, list(map.keys()), 20, 0.01)
